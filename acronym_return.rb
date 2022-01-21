d5c4db4e701f4986f96067344db7792770409b39
Andre Menezes <djdedemenezes@gmail.com>
2022-01-21 11:15:36 -0300

    comments on livecode

--- acronym_return.rb
+++ acronym_return.rb
@@ -1,20 +1,22 @@
 def acronymize(sentence)
-  return "" if sentence.empty?
-  # code IF condition == true
+  # run_this_code IF condition == true
+  return '' if sentence.empty?
 
   # split the sentence on white spaces store in words variable
-  words = sentence.split(" ")
+  words = sentence.split(' ')
   # iterate over words, for each, select the first letters
   acronym = words.map do |word|
-    upcased_letter = word[0]
+    word[0]
   end
   # join the words array and  upcase
   acronym.join.upcase
 end
 
-#######
-new_array = []
+### If you spot this pattern    ###
+### think about which iterator  ###
+### you can use instead of each ###
+
+# new_array = []
 # elements.each do |element|
 #   new_array << element
 # end
-#######
